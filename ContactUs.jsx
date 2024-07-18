4489e3c9233f48b5ee7067c0ecacda9b2064f478
SnehaVerma24 <168652282+SnehaVerma24@users.noreply.github.com>
2024-07-18 18:56:30 +0530

    Add files via upload

---
+++ ContactUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactUs(){
+    return(
+    <div>
+        <p>Call us at:987654321</p>
+        <p>Mail us at:abc@gmail.com</p>
+    </div>
+    )
+}
