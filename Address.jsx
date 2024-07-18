4060a48185c3f275bee1178d6a21e20e018ec9ba
SnehaVerma24 <168652282+SnehaVerma24@users.noreply.github.com>
2024-07-18 18:55:52 +0530

    Add files via upload

---
+++ Address.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Address(){
+    return(
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sunt ipsam nisi illo tempore maxime rerum id laboriosam ullam ipsum?</p>
+    </div>
+    )
+}
