9970b3ffec1589626c69f88f72f0eb1eeb2e0e32
SnehaVerma24 <168652282+SnehaVerma24@users.noreply.github.com>
2024-07-24 19:09:28 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,38 +1,24 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Registration from './components/Registration.jsx'
 
-import Home from './components/pages/Home.jsx'
-import About from './components/pages/About.jsx'
-import Contact from './components/pages/Contact.jsx'
-import Products from './components/pages/Products.jsx'
-import ContactUs from './components/pages/ContactUs.jsx'
-import Address from './components/pages/Address.jsx'
-
-
-
-import {createBrowserRouter, Route, createRoutesFromElements, RouterProvider} from  'react-router-dom'
+import {Route,RouterProvider,createBrowserRouter,createRoutesFromElements} from 'react-router-dom'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-    <Route index element={<Home/>}/>
-      <Route path="about" element={<About/>}/>
-      <Route path="contact" element={<Contact/>}>
-          <Route path="contactus" element={<ContactUs/>}/>
-          <Route path="address" element={<Address/>}/>
-      </Route>
-      <Route path="products" element={<Products/>}/>
+
+    <Route path="/" >
+      <Route path="" element={<App/>}/>
+      <Route path="registration" element={<Registration/>}/>
     </Route>
-  
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
